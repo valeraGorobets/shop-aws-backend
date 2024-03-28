8fc17d8b75233936482dbf41b4854585bf33082c
Valera Gorobets <valera.gorobets@gmail.com>
2024-03-28 10:16:02 +0200

    feat: implement seed script to populate db in aws

---
+++ product-service/src/db/seed/seed-db.mjs
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const TableId = {
+	'Products': 'products',
+	'Stocks': 'stocks',
+};
+
+const TableIdToFileName = {
+	[TableId.Products]: 'products.seed.json',
+	[TableId.Stocks]: 'stocks.seed.json',
+};
+
+const TableIdToDynamoDBTableName = {
+	[TableId.Products]: 'ProductsTable',
+	[TableId.Stocks]: 'StocksTable',
+};
+
+const dynamodbClient = new DynamoDBClient({
+	region: 'eu-west-1',
+});
+const documentClient = DynamoDBDocumentClient.from(dynamodbClient);
+
+(async function run() {
+	for (const tableId of Object.values(TableId)) {
+		await seedTable(tableId);
+		console.log(`Seeding table ${tableId} finished`)
+	}
+})();
+
+async function seedTable(tableId) {
+	const fileDir = path.join(__dirname, TableIdToFileName[tableId]);
+	const jsonData = fs.readFileSync(fileDir, 'utf8');
+	const data = JSON.parse(jsonData);
+	await seedDynamoDB(tableId, data);
+}
+
+async function seedDynamoDB(tableId, data) {
+	try {
+		for (const item of data) {
+			const putParams = {
+				TableName: TableIdToDynamoDBTableName[tableId],
+				Item: item
+			};
+			await documentClient.send(new PutCommand(putParams));
+		}
+	} catch (error) {
+		console.error('Error seeding DynamoDB:', error);
+	}
+}
